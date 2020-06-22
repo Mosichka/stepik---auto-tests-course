467e9393c70e72ec5c4311b4d3169ba6bd6aa20a
Mosichka <36281187+Mosichka@users.noreply.github.com>
2020-06-22 13:39:05 -0500

    Homework 2.3.3

---
+++ python 2.3.3.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/alert_accept.html")
+
+    button = browser.find_element_by_css_selector(".btn.btn-primary")
+    button.click()
+
+    alert = browser.switch_to.alert
+    alert.accept()
+
+    x_element = browser.find_element_by_css_selector("#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+
+    button = browser.find_element_by_css_selector(".btn.btn-primary")
+    button.click()
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
